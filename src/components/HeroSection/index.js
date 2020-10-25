112824a9194418944fd24a731d85c21c09a1abcc
aashishuppal <rogerux09@gmail.com>
2020-10-25 15:18:49 +0530

    responsive website in react using styled component

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import { ArrowForward, ArrowRight, HeroBg, HeroBtnWrapper, HeroContainer, HeroContent, HeroH1, HeroP, VideoBg } from './HeroElements'
+import {Button} from '../ButtonElement'
+import Video from '../../videos/video.mp4'
+
+const HeroSection = () => {
+
+const [hover,setHover] = useState(false)
+
+const onHover = () => {
+    setHover(!hover)
+}
+
+
+    return (
+        <HeroContainer id='home'>
+            <HeroBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Virtual Banking Made Easy
+                </HeroH1>
+                <HeroP>
+                    Sign Up for a new account today and receive $250 in credit towards your next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true' smooth={true} duration={500} spy={true} exact='true'>Get started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+            
+        </HeroContainer>
+    )
+}
+
+export default HeroSection
